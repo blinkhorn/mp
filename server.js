4ad015fe239aa0db47b94a86a7272406af425e5a
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2019-08-30 19:32:20 -0400

    server up

--- server.js
+++ server.js
@@ -5,3 +5,14 @@ const app = express();
 
 // connect database
 connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }))
+
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
