fc23966a40df2480f41ffc91a3ea512f81277874
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2019-08-31 14:42:24 -0400

    Dashboard added

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import PrivateRoute from './components/routing/PrivateRoute';
 import Navbar from './components/layout/Navbar.js';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Dashboard from './components/dashboard/Dashboard';
+
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
