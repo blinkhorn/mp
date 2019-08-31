e1266202c05e3a9bc3988d01687160fff06ad6c6
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2019-08-31 13:32:31 -0400

    cleanup and basics for App.ks

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Redux
+import { Provider } from 'react-redux';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // <Provider store={store}>
+        <Router>
+            <Fragment>
+                <Navbar />
+                <Route exact path="/" component={Landing} />
+                <section className="container">
+                    <Alert />
+                    <Switch>
+                        <Route
+                            exact
+                            path="/register"
+                            component={Register}
+                        />
+                        <Route exact path="/login" component={Login} />
+                        {/* <PrivateRoute exact path="/dashboard" component={Dashboard} /> */}
+                    </Switch>
+                </section>
+            </Fragment>
+        </Router>
+    // </Provider>
+);
 }
 
 export default App;
