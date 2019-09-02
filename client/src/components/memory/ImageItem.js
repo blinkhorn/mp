52e391ef4359f3282f82f35a6a6fa7c2bf7259b2
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2019-09-02 12:44:24 -0400

    card flip enabled

--- client/src/components/memory/ImageItem.js
+++ client/src/components/memory/ImageItem.js
@@ -7,30 +7,49 @@ import { deleteImage } from '../../actions/memory';
 // Material
 import Button from '@material-ui/core/Button';
 
+// CSS
+import '../../App.css'
+
 const ImageItem = ({
     memoryId,
     image: { _id, user, memory, imageURL, name, date },
     auth,
     deleteImage
 }) => (
-    <div>
-        <div>
-            <h3>{memory}</h3>
-            <img src={imageURL} alt={`An association for ${memory}`}></img>
-
-            <p>
-                Created by {name} on <Moment format="MM/DD/YYYY">{date}</Moment>
-            </p>
-            {!auth.loading && user === auth.user._id && (
-                <Button
-                    variant="contained"
-                    color="secondary"
-                    onClick={() => deleteImage(memoryId, _id)}
-                    type="button"
-                >
-                    Delete Image
-                </Button>
-            )}
+    <div
+        className="flip-container"
+    >
+        <div className="flipper">
+            <div className="front">
+                <img src={imageURL} alt={`An association for ${memory}`}></img>
+                {!auth.loading && user === auth.user._id && (
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={() => deleteImage(memoryId, _id)}
+                        type="button"
+                    >
+                        Delete
+                    </Button>
+                )}
+            </div>
+            <div className="back">
+                <h3>{memory}</h3>
+                <p>
+                    Created by {name} on{' '}
+                    <Moment format="MM/DD/YYYY">{date}</Moment>
+                </p>
+                {!auth.loading && user === auth.user._id && (
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={() => deleteImage(memoryId, _id)}
+                        type="button"
+                    >
+                        Delete Image
+                    </Button>
+                )}
+            </div>
         </div>
     </div>
 );
