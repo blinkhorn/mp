9b0b295479e3226c2126ffa08f1ad74b554aa2db
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2019-11-11 19:48:54 -0500

    user can only see their own memories

--- client/src/components/memories/Memories.js
+++ client/src/components/memories/Memories.js
@@ -1,44 +1,45 @@
-import React, { Fragment, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import MemoryItem from './MemoryItem';
-import MemoryForm from './MemoryForm';
-import { getMemories } from '../../actions/memory';
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import MemoryItem from "./MemoryItem";
+import MemoryForm from "./MemoryForm";
+import { getMemories } from "../../actions/memory";
 
 // CSS
-import '../../App.css';
+import "../../App.css";
 
-const Memories = ({ getMemories, memory: { memories, loading } }) => {
-    useEffect(() => {
-        getMemories();
-    }, [getMemories]);
+const Memories = ({ getMemories, auth, memory: { memories, loading } }) => {
+  useEffect(() => {
+    getMemories();
+  }, [getMemories]);
 
-    return loading ? (
-        <div></div>
-    ) : (
-        <Fragment>
-            <h1>Memories</h1>
-            <p>Welcome to MP!</p>
-            <div className="memory-container">
-                <MemoryForm />
-                {memories.map(memory => (
-                    <MemoryItem key={memory._id} memory={memory} />
-                ))}
-            </div>
-        </Fragment>
-    );
+  return loading ? (
+    <div></div>
+  ) : (
+    <Fragment>
+      <h1>Memories</h1>
+      <p>Welcome to MP!</p>
+      <div className="memory-container">
+        <MemoryForm />
+        {memories
+          .filter(memory => !auth.loading && memory.user === auth.user._id)
+          .map(memory => (
+            <MemoryItem key={memory._id} memory={memory} />
+          ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Memories.propTypes = {
-    getMemories: PropTypes.func.isRequired,
-    memory: PropTypes.object.isRequired
+  getMemories: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  memory: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-    memory: state.memory
+  memory: state.memory,
+  auth: state.auth
 });
 
-export default connect(
-    mapStateToProps,
-    { getMemories }
-)(Memories);
+export default connect(mapStateToProps, { getMemories })(Memories);
